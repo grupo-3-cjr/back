80fa7b60b9539bdfde0cf1b75c63c4fd191f7746
Fabio-AS06 <willianfabio549@gmail.com>
2026-05-08 02:36:13 -0300

    feat: "arquivos auth"

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    login() {
+       // return this.authService.login();
+    }
+}
