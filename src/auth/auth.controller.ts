dc74867c675b68aae425f46c77e1c87ded8cfb1d
Fabio-AS06 <willianfabio549@gmail.com>
2026-05-08 12:33:24 -0300

    feat: "endpoint login"

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('login')
     @HttpCode(HttpStatus.OK)
+    @UseGuards(LocalAuthGuard)
     login() {
        // return this.authService.login();
     }
