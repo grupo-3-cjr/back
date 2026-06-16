46c6204291851f44b9c2fefbad7af93fb5ba990b
João Francisco <jhonjhon2sj@gmail.com>
2026-06-15 21:41:09 -0300

    feat: configurando para deletar e atualizar imagem no cloud

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UploadService } from 'src/upload/upload.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, UploadService],
   exports: [UserService],
 })
 export class UserModule {}
