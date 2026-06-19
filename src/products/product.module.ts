1cd9f50e47ba6fd257649f479e6f259e45fa4100
João Francisco <jhonjhon2sj@gmail.com>
2026-06-19 11:36:06 -0300

    refactor: banco povoado

--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
+  imports: [
+    UploadModule 
+  ],
   controllers: [ProductController],
   providers: [ProductService,PrismaService],
 })
